70a7350c7306740cd71bab7258e261c1f9bbb781
jintak0401 <32933980+jintak0401@users.noreply.github.com>
2020-12-24 16:07:20 +0900

    메타태그 수정

--- components/layout/header.js
+++ components/layout/header.js
@@ -71,10 +71,12 @@ function Header({ t }) {
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-HJ76HWLCTC"
         />
+        <meta name="Keywords" content="인터클린, interclean, 세차, 중형차" />
         <meta
-          name="description"
+          name="Description"
           content="인터클린은 중차량, 버스, 트럭 및 철도차량 세척 시스템 전문 설계 및 제작회사입니다."
         />
+        <meta http-equiv="Title" content="인터클린 코리아" />
       </Head>
       <div className={styles.header__top}>
         <div className="container">
