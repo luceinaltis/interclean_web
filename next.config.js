cd062bb3ad7bc893156c254d7524b71b64ba4dde
luceinaltis <luceinaltis2020@naver.com>
2020-12-31 01:17:45 +0900

    update

--- next.config.js
+++ next.config.js
@@ -4,24 +4,21 @@ const { nextI18NextRewrites } = require("next-i18next/rewrites");
 const localeSubpaths = {};
 
 module.exports = {
-  rewrites: async () => nextI18NextRewrites(localeSubpaths),
-  publicRuntimeConfig: {
-    localeSubpaths,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-        net: "empty",
-        tls: "empty",
-      };
-    }
-    return config;
-  },
-  env: {
-    SERVICE_ID: process.env.SERVICE_ID,
-    TEMPLATE_ID: process.env.TEMPLATE_ID,
-    USER_ID: process.env.USER_ID,
-    ACCESS_TOKEN: process.env.ACCESS_TOKEN,
-  },
+    rewrites: async () => nextI18NextRewrites(localeSubpaths),
+    publicRuntimeConfig: {
+        localeSubpaths,
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.node = {
+                fs: "empty",
+                net: "empty",
+                tls: "empty",
+            };
+        }
+        return config;
+    },
+    env: {
+        EMAILJS_USER_ID: process.env.EMAILJS_USER_ID,
+    },
 };
